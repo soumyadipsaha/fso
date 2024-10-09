fdb2cca972c3d5e573e3bcc977e2e08ed0527f62
SS <soumyadipsaha45@gmail.com>
2024-10-10 02:54:11 +0530

    1.9 added conditional rendering

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -14,12 +14,14 @@ const App = () => {
     return(
       <>
       <h1>Statistics</h1>
-      <p>Good: {good}</p>
+      {good+bad+neutral === 0? (<p>No Feedback Given</p>) :(  <><p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
       <p>All: {good+neutral+bad}</p>
       <p>Average: {(good-bad)/(good+neutral+bad)}</p>
-      <p>Positive: {(good/(good+neutral+bad))*100} %</p>
+      <p>Positive: {(good/(good+neutral+bad))*100} %</p></>)}
+  
+    
       </>
     )
 

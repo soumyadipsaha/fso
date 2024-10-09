32006e5d71a265aefff53bb72d1fcff84cd2d151
SS <soumyadipsaha45@gmail.com>
2024-10-10 02:05:11 +0530

    1.3 introduced objects

--- part1/course-information/src/Components/Content.jsx
+++ part1/course-information/src/Components/Content.jsx
@@ -2,18 +2,13 @@ import React from 'react'
 import Part from './Part'
 const Content = (props) => {
 
-    const part1 = props.part1
-    const exercises1 = props.exercises1
-    const part2 = props.part2
-    const exercises2 = props.exercises2
-    const part3 = props.part3
-    const exercises3 = props.exercises3
+    const data = props.data
     
   return (
     <>
-      <Part part={part1} exercises ={exercises1}/>
-      <Part part={part2} exercises ={exercises2}/>
-      <Part part={part3} exercises ={exercises3}/>
+      {data.map((part, index) => {
+        return <Part key={index} part={part.name} exercises={part.exercises} />
+      })}
       
     </>
   )

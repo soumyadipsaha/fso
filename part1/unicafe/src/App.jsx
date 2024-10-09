5e696678ed20619d7a6edd2b3ee63dade71da89a
SS <soumyadipsaha45@gmail.com>
2024-10-10 02:47:37 +0530

    1.8 step 3 made statistics component

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,13 +6,13 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  return (
-    <>
-      <h1>Give Feedback</h1>
-      <button onClick={()=>setGood(good+1)}>Good</button>
-      <button onClick ={()=>setNeutral(neutral+1)} >Neutral</button>
-      <button onClick = {()=> setBad(bad+1)}>Bad</button>
+  
 
+  const Statistics = (props)=>{
+    const {good, neutral, bad} = props
+
+    return(
+      <>
       <h1>Statistics</h1>
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
@@ -20,6 +20,23 @@ const App = () => {
       <p>All: {good+neutral+bad}</p>
       <p>Average: {(good-bad)/(good+neutral+bad)}</p>
       <p>Positive: {(good/(good+neutral+bad))*100} %</p>
+      </>
+    )
+
+  }
+
+
+  return (
+    <>
+      <h1>Give Feedback</h1>
+      <button onClick={()=>setGood(good+1)}>Good</button>
+      <button onClick ={()=>setNeutral(neutral+1)} >Neutral</button>
+      <button onClick = {()=> setBad(bad+1)}>Bad</button>
+      <Statistics 
+        good = {good}
+        bad = {bad}
+        neutral = {neutral}  />
+      
 
     </>
   )

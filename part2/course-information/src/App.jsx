e1938f86122ccc5ec231fc5993ec44a83c98c0c2
SS <soumyadipsaha45@gmail.com>
2024-10-10 16:32:41 +0530

    2.5 & 2.4 separate Course component and course array

--- part2/course-information/src/App.jsx
+++ part2/course-information/src/App.jsx
@@ -3,39 +3,65 @@ import Course from './components/Course'
 
 const App = () => {
 
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  const ExercisesTotal = () => {
-    
-    const exerciseSum = course.parts.reduce((sum, part) => sum + part.exercises, 0)
+  const ExercisesTotal = ({ parts }) => {
+    const exerciseSum = parts.reduce((sum, part) => sum + part.exercises, 0)
 
-    return( <p><strong>Total number of exercises {exerciseSum}</strong></p>)
+    return( <p><strong>Total number of exercises: {exerciseSum}</strong></p>)
   }
 
   return (
     <>
-      <Course course={course} />
-      <ExercisesTotal />
+      {courses.map(course => (
+        <div key={course.id}>
+          <Course course={course} />
+          <ExercisesTotal parts={course.parts} />
+        </div>
+      ))}
     </>
   )
 }

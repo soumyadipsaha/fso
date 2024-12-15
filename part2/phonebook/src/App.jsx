e93d2530780398938f277518b4dcfd5154aa5aa4
SS <soumyadipsaha45@gmail.com>
2024-12-16 00:20:31 +0530

    3.9 code works with the backend now

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,138 +1,157 @@
-import { useState , useEffect} from 'react'
-import PersonForm from './Components/PersonForm'
-import Filter from './Components/Filter'
-import Persons from './Components/Persons'
-import {createPerson, getAll, deletePerson, updatePerson} from './services/persons'
-import Notification from './Components/Notification'
-import './App.css'
+import { useState, useEffect } from "react"
+import PersonForm from "./Components/PersonForm"
+import Filter from "./Components/Filter"
+import Persons from "./Components/Persons"
+import {
+    createPerson,
+    getAll,
+    deletePerson,
+    updatePerson,
+} from "./services/persons"
+import Notification from "./Components/Notification"
+import "./App.css"
 const App = () => {
-  const [persons, setPersons] = useState([]) 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [filter, setFilter] = useState('')
-  const [errorMessage, setErrorMessage] = useState(null)
+    const [persons, setPersons] = useState([])
+    const [newName, setNewName] = useState("")
+    const [newNumber, setNewNumber] = useState("")
+    const [filter, setFilter] = useState("")
+    const [errorMessage, setErrorMessage] = useState(null)
 
+    useEffect(() => {
+        getAll()
+            .then((response) => {
+                setPersons(response.data)
+            })
+            .catch((error) => {
+                alert("Load data failed")
+            })
+    }, [])
 
-  useEffect(() => {
-    getAll()
-      .then(response =>{
-        setPersons(response.data)
-      })
-      .catch(error=>{
-        alert("Load data failed")
-      })
-  },[])
+    const handleSubmit = (e) => {
+        e.preventDefault()
 
-  const handleSubmit= (e)=>{
-    e.preventDefault()
+        const existingPerson = persons.find((person) => person.name === newName)
 
-    const existingPerson = persons.find(person => person.name === newName);
+        if (existingPerson) {
+            if (
+                !window.confirm(
+                    `${newName} is already in the phonebook, do you want to replace the number?`
+                )
+            ) {
+                return
+            }
 
-if (existingPerson) {
-  if (!window.confirm(`${newName} is already in the phonebook, do you want to replace the number?`)) {
-    return;
-  }
+            const updatedPerson = { ...existingPerson, number: newNumber }
 
-  const updatedPerson = { ...existingPerson, number: newNumber };
+            updatePerson(existingPerson.id, updatedPerson)
+                .then((response) => {
+                    setPersons(
+                        persons.map((person) =>
+                            person.id === existingPerson.id
+                                ? response.data
+                                : person
+                        )
+                    )
+                    setErrorMessage({
+                        error: false,
+                        message: `${response.data.name} updated successfully`,
+                    })
+                    setTimeout(() => setErrorMessage(null), 5000)
+                })
+                .catch((error) => {
+                    alert("Operation Failed")
+                })
 
-
-  updatePerson(existingPerson.id, updatedPerson)
-    .then(response => {
-      setPersons(persons.map(person => 
-        person.id === existingPerson.id ? response.data : person
-      ))
-      setErrorMessage({error: false, message:`${response.data.name} updated successfully`})
-      setTimeout(()=>setErrorMessage(null), 5000)
-    })
-    .catch(error => {
-      alert("Operation Failed");
-    })
-
-  return;
-}
-
-
-    const addPerson ={
-      name: newName,
-      number: newNumber
-    }
-
-    createPerson(addPerson)
-      .then(response=>{
-        setErrorMessage({error: false, message:`${response.data.name} added successfully`})
-        setTimeout(()=>setErrorMessage(null), 5000)
-        console.log(response)
-         setPersons(persons.concat(response.data))
-      })
-      .catch(error=>{
-        alert("Operation Failed")
-      })
-    
-    setNewName('')
-    setNewNumber('')
-  }
-
-  const handleNameInput = (e)=>{
-    setNewName(e.target.value)
-    
-  }
-
-  const handleNumberInput = (e)=>{
-    setNewNumber(e.target.value)
-  }
-
-  const handleFilterInput = (e)=>{
-    setFilter(e.target.value)
-
-  }
-
-  const handleDelete = (toDelete)=>{
-  
-    if(!(window.confirm(`Delete ${toDelete.name} ?`))){
-      return
-    }
-    console.log(toDelete)
-
-    deletePerson(toDelete.id)
-      .then(response=>{
-        setPersons(persons.filter(person => person.id !== toDelete.id))
-        setErrorMessage({error: false, message:`${toDelete.name} deleted successfully`})
-        setTimeout(()=>setErrorMessage(null), 5000)
-      })
-      .catch(error=>{
-
-        if(error.response.status === 404){
-          setErrorMessage({error: true, message:`Information of ${toDelete.name} has already been removed from server`})
-          setPersons(persons.filter(person => person.id !== toDelete.id))
-          setTimeout(()=>setErrorMessage(null), 5000)
-          return
+            return
         }
-        setErrorMessage({error: true, message:`Operation Failed`})
-          setTimeout(()=>setErrorMessage(null), 5000)
-      })
-  }
 
-  return (
-    <div>
-      <Notification message={errorMessage}/>
-      <Filter 
-        handleFilterInput={handleFilterInput} 
-      />
-      <PersonForm 
-        handleSubmit={handleSubmit} 
-        newName={newName} 
-        handleNameInput={handleNameInput} 
-        newNumber={newNumber} 
-        handleNumberInput={handleNumberInput} 
-      />
-      <Persons 
-        persons={persons} 
-        filter={filter}
-        handleDelete={handleDelete}
-      />
-      
-    </div>
-  )
+        const addPerson = {
+            name: newName,
+            number: newNumber,
+        }
+
+        createPerson(addPerson)
+            .then((response) => {
+                setErrorMessage({
+                    error: false,
+                    message: `${response.data.name} added successfully`,
+                })
+                setTimeout(() => setErrorMessage(null), 5000)
+                console.log(response)
+                setPersons(persons.concat(response.data))
+            })
+            .catch((error) => {
+                alert("Operation Failed")
+            })
+
+        setNewName("")
+        setNewNumber("")
+    }
+
+    const handleNameInput = (e) => {
+        setNewName(e.target.value)
+    }
+
+    const handleNumberInput = (e) => {
+        setNewNumber(e.target.value)
+    }
+
+    const handleFilterInput = (e) => {
+        setFilter(e.target.value)
+    }
+
+    const handleDelete = (toDelete) => {
+        if (!window.confirm(`Delete ${toDelete.name} ?`)) {
+            return
+        }
+        console.log(toDelete)
+
+        deletePerson(toDelete.id)
+            .then((response) => {
+                setPersons(
+                    persons.filter((person) => person.id !== toDelete.id)
+                )
+                setErrorMessage({
+                    error: false,
+                    message: `${toDelete.name} deleted successfully`,
+                })
+                setTimeout(() => setErrorMessage(null), 5000)
+            })
+            .catch((error) => {
+                if (error.response.status === 404) {
+                    setErrorMessage({
+                        error: true,
+                        message: `Information of ${toDelete.name} has already been removed from server`,
+                    })
+                    setPersons(
+                        persons.filter((person) => person.id !== toDelete.id)
+                    )
+                    setTimeout(() => setErrorMessage(null), 5000)
+                    return
+                }
+                setErrorMessage({ error: true, message: `Operation Failed` })
+                setTimeout(() => setErrorMessage(null), 5000)
+            })
+    }
+
+    return (
+        <div>
+            <Notification message={errorMessage} />
+            <Filter handleFilterInput={handleFilterInput} />
+            <PersonForm
+                handleSubmit={handleSubmit}
+                newName={newName}
+                handleNameInput={handleNameInput}
+                newNumber={newNumber}
+                handleNumberInput={handleNumberInput}
+            />
+            <Persons
+                persons={persons}
+                filter={filter}
+                handleDelete={handleDelete}
+            />
+        </div>
+    )
 }
 
 export default App
--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,15 +1,15 @@
 import axios from "axios"
-const baseUrl = "http://localhost:3001/persons"
+const baseUrl = "http://localhost:3001/api/persons"
 
+//works with backend
 export const getAll = () => {
-    const request = axios.get(baseUrl)
     return axios.get(baseUrl)
 }
-
+//works with backend
 export const createPerson = (newObject) => {
     return axios.post(baseUrl, newObject)
 }
-
+//works wirh backend
 export const deletePerson = (id) => {
     return axios.delete(`${baseUrl}/${id}`)
 }
@@ -17,5 +17,3 @@ export const deletePerson = (id) => {
 export const updatePerson = (id, newObject) => {
     return axios.put(`${baseUrl}/${id}`, newObject)
 }
-
-
